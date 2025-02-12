d4c9fb5e6ea3a849f7c791ef1230f98b51e8b181
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2025-02-11 18:47:39 -0600

    add search logic

---
+++ composeApp/src/commonMain/kotlin/org/hasancobanoglu/bookapp/data/network/KtorRemoteBookDataSource.kt
@@ -0,0 +1,34 @@
+package org.hasancobanoglu.bookapp.data.network
+
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import org.hasancobanoglu.bookapp.data.dto.SearchResponseDto
+import org.hasancobanoglu.bookapp.domain.Book
+import org.hasancobanoglu.core.data.safeCall
+import org.hasancobanoglu.core.domain.DataError
+import org.hasancobanoglu.core.domain.Result
+
+private const val BASE_URL = "https://openlibrary.org"
+
+class KtorRemoteBookDataSource(
+    private val httpClient: HttpClient
+) {
+
+    suspend fun searchBooks(
+        query: String,
+        resultLimit: Int?
+    ): Result<SearchResponseDto, DataError.Remote> {
+        return safeCall<SearchResponseDto> {
+            httpClient.get(
+                urlString = "$BASE_URL/search.json"
+            ) {
+                parameter("q", query)
+                parameter("limit", resultLimit)
+                parameter("language", "eng")
+                parameter("fields", "key,title,author_name,author_key,cover_edition_key,cover_i,ratings_average,ratings_count,first_publish_year,language,number_of_pages_median,edition_count")
+            }
+        }
+    }
+
+}
