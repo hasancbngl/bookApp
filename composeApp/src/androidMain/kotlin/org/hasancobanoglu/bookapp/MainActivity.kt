f567926313792f3be3692179f5993dde13d8e2e2
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2025-02-19 17:11:19 -0600

    refactor

--- composeApp/src/androidMain/kotlin/org/hasancobanoglu/bookapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/hasancobanoglu/bookapp/MainActivity.kt
@@ -4,16 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
-import okhttp3.OkHttp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App(engine = remember { io.ktor.client.engine.okhttp.OkHttp.create() })
+            App()
         }
     }
 }
--- composeApp/src/commonMain/kotlin/org/hasancobanoglu/bookapp/App.kt
+++ composeApp/src/commonMain/kotlin/org/hasancobanoglu/bookapp/App.kt
@@ -2,32 +2,20 @@ package org.hasancobanoglu.bookapp
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import io.ktor.client.engine.HttpClientEngine
-import org.hasancobanoglu.bookapp.data.network.KtorRemoteBookDataSource
-import org.hasancobanoglu.bookapp.data.network.RemoteBookDataSource
-import org.hasancobanoglu.bookapp.data.repository.BookRepository
-import org.hasancobanoglu.bookapp.domain.AppRepository
 import org.hasancobanoglu.bookapp.presentation.book_list.BookListScreenRoot
 import org.hasancobanoglu.bookapp.presentation.book_list.BookListViewModel
-import org.hasancobanoglu.core.data.HttpClientFactory
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 @Preview
-fun App(engine: HttpClientEngine) {
+fun App() {
     MaterialTheme {
         BookListScreenRoot(
-            viewModel = remember {
-                BookListViewModel(
-                    repository = BookRepository(
-                        remoteBookDataSource = KtorRemoteBookDataSource(
-                            HttpClientFactory.create(engine)
-                        )
-                    )
-                )
-            },
-            onBookClick = {}
+            viewModel = koinViewModel<BookListViewModel>(),
+            onBookClick = {
+
+            }
         )
     }
 }
