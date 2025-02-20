24c96198dedfc774f1333110499d1ce2473f3c1a
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2025-02-19 18:00:05 -0600

    commit changes

--- composeApp/src/commonMain/kotlin/org/hasancobanoglu/bookapp/app/App.kt
+++ composeApp/src/commonMain/kotlin/org/hasancobanoglu/bookapp/app/App.kt
@@ -1,9 +1,14 @@
 package org.hasancobanoglu.bookapp.app
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import org.hasancobanoglu.bookapp.presentation.book_list.BookListScreenRoot
 import org.hasancobanoglu.bookapp.presentation.book_list.BookListViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -14,13 +19,26 @@ import org.koin.compose.viewmodel.koinViewModel
 fun App() {
     MaterialTheme {
         val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = )
-
-        BookListScreenRoot(
-            viewModel = koinViewModel<BookListViewModel>(),
-            onBookClick = {
-
+        NavHost(
+            navController = navController,
+            startDestination = Route.BookGraph
+        ) {
+            navigation<Route.BookGraph>(
+                startDestination = Route.BookList
+            ) {
+                composable<Route.BookList> {
+                    BookListScreenRoot(
+                        viewModel = koinViewModel<BookListViewModel>(),
+                        onBookClick = { book ->
+                            navController.navigate(Route.BookDetail(book.id))
+                        }
+                    )
+                }
+                composable<Route.BookDetail> { entry ->
+                    val args = entry.toRoute<Route.BookDetail>()
+                    Text("Book Detail: ${args.id}")
+                }
             }
-        )
+        }
     }
 }
